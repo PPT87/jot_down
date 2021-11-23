e44aee1d195f048780b0ce44de7eacdf54408bfe
PeterT <tranpp87@gmail.com>
2021-11-22 23:22:27 -0500

    add model and create superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Jot(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  task = models.CharField(max_length=200)
+  complete = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.task
+
+  class Meta:
+    ordering =['complete']
