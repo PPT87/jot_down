e367231d737da2972d19c8c42965c296f684f3ed
PeterT <tranpp87@gmail.com>
2021-11-23 13:44:07 -0500

    add navbar

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Jot
+from .models import Jot, SubJot
 
 
 # Create your views here.
 class JotList(ListView):
   model = Jot
+  context_object_name = 'jots'
+
+class SubJotList(ListView):
+  model = SubJot
