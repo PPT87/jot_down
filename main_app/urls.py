92d43bfacc3cdbd8b4c29b8874e4dc882943370e
PeterT <tranpp87@gmail.com>
2021-11-23 22:02:35 -0500

    update navbar and add homepage

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.task_list, name='tasks'),
+  path('', views.Home.as_view(), name='home'),
+  path('jots/', views.task_list, name='tasks'),
   path('about/', views.about, name='about'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
 from .models import Task, SubTask
 from .forms import TaskForm
 # from django.urls import reverse
 
 
 # Create your views here.
+class Home(LoginView):
+  template_name = 'home.html'
+
 def task_list(request):
   tasks = Task.objects.all()
 
@@ -14,7 +18,7 @@ def task_list(request):
     form = TaskForm(request.POST)
     if form.is_valid():
       form.save()
-    return redirect('/')
+    return redirect('/jots')
   context = {'tasks':tasks, 'form':form}
   return render(request, 'jots/task_list.html', context)
 
