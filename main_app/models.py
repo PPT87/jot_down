30f0956c8ca345b7987a9e60bfe1bca1d883b38f
PeterT <tranpp87@gmail.com>
2021-11-27 13:13:04 -0500

    update model return reverse to redirect to details after adding subjot

--- main_app/models.py
+++ main_app/models.py
@@ -27,7 +27,7 @@ class SubJot(models.Model):
     return '%s - %s' % (self.jot.title, self.title)
 
   def get_absolute_url(self):
-    return reverse('jots_detail')
+    return reverse('jots_detail', kwargs={'jot_id': self.jot.id})
 
   class Meta:
     ordering =['complete']
